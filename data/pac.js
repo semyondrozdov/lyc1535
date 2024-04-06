3ceb96cb28fc0dc7247c371bc861edd76e7751a1
Семён Дроздов <90418888+semyondrozdov@users.noreply.github.com>
2024-04-06 23:46:23 +0300

    Update pac.js

--- data/pac.js
+++ data/pac.js
@@ -1,3 +1,6 @@
 function FindProxyForURL(url, host) {
-	return "PROXY proxy.lyc1535.ru:3128";
+	if (dnsDomainIs(host, "online1535.ru") || dnsDomainIs(host, "*.online1535.ru")) {
+		return "PROXY proxy.lyc1535.ru:3128";
+	}
+	return "DIRECT";
 }
