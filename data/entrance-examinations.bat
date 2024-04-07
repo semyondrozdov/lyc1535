3a3092d7e57497035062391cf2dcb7ac88b50088
Семён Дроздов <90418888+semyondrozdov@users.noreply.github.com>
2024-04-07 08:24:57 +0300

    Update entrance-examinations.bat

--- data/entrance-examinations.bat
+++ data/entrance-examinations.bat
@@ -1,2 +1,2 @@
 taskkill /f /im chrome.exe
-start chrome --incognito --new-window --proxy-pac-url="https://static.lyc1535.ru/data/pac.js"
+start chrome --incognito --new-window --proxy-pac-url="https://static.lyc1535.ru/data/pac.js" --start-fullscreen
